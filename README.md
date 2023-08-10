c855e26b6eedfe42f8212d6bdc0671ac3c16e5a1
sahuno <ekwame001@gmail.com>
2023-08-10 17:46:30 -0400

    updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # methyl_longRead_wf
-a snakemake workflow to compute methylation of long read sequences and associate with gene expression
+a snakemake workflow to compute methylation activity of genes with long read sequencing and associate with gene expression
 
+## how to install pipeline
+```
+$ git clone git@github.com:sahuno/methyl_longRead_wf.git
+$ cd methyl_longRead_wf
+# option: install snakemake conda environment 
+$ conda activate snakemake #activate conda env
+
+#MUST: edit `config/samples.yaml` to match paths to per_read_modified_base.txt of each sample
+
+$ sh run_snakefile.sh
+```
 ## inputs
-1. samples.yaml file speciying sample name and file paths to megalodon per read modified bases file
+1. `samples.yaml` file specifying sample name and file paths to megalodon per read modified bases file
 2. 
+
+## QC 
+there are a number of qc available
+1. depth of sequencing per CpG site
--- scripts/R/make_sample_yaml.r
+++ scripts/R/make_sample_yaml.r
@@ -1,21 +1,34 @@
 #make sample list files
-library(RJSONIO)
 library(tidyverse)
 library(data.table)
 library(yaml)
 
+
 path_megalodon_dir <- "/juno/work/greenbaum/projects/TRI_EPIGENETIC/megalodon"
 files_mod_bases <- list.files(path_megalodon_dir, full.names= TRUE, pattern="er_read_modified_base_calls.txt", recursive = TRUE) %>% as_tibble()
-
+files_mod_bases
 
 files_mod_bases2yaml <- files_mod_bases %>% mutate(samples = gsub("/per_read_modified_base_calls.txt","",gsub(".*megalodon/","",value))) %>% as.data.table()
+#names(files_mod_bases2yaml) <- NULL
+
+# cat(yaml::as.yaml(list(samples = split(replace(files_mod_bases2yaml, "samples", NULL), f = files_mod_bases2yaml$samples))))
+# cat(yaml::as.yaml(list(samples = split(files_mod_bases2yaml, f = files_mod_bases2yaml$samples))))
+# split(files_mod_bases2yaml, f = files_mod_bases2yaml$samples)
+
+
+files_mod_bases2yaml <- files_mod_bases2yaml %>% mutate(samples= paste0(samples, ": ", value )) %>% dplyr::select(-value) %>% as.data.table()
+write_yaml(files_mod_bases2yaml, file="files_mod_bases2yaml.yaml")
+
+
+
+
 
 # files_mod_bases2yaml <- split(files_mod_bases2yaml, f = files_mod_bases2yaml$samples)
+# names(files_mod_bases2yaml) <- NULL
 # files_mod_bases2yaml <- files_mod_bases2yaml %>% split(by="samples") %>%
 #   lapply(function(x) x[,sample := NULL] %>% .[])
 
 # cat(yaml::as.yaml(files_mod_bases2yaml))
 
 
-write_yaml(files_mod_bases2yaml, file="files_mod_bases2yaml.yaml")
 
